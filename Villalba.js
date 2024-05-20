67c7034e706abf20f7c5f8d61a6f96bb6732d215
Nare <narela00528@gmail.com>
2024-05-20 18:47:31 -0300

    first commit

---
+++ Villalba.js
@@ -0,0 +1,36 @@
+
+let producto = {
+    nombre: "Camiseta",
+    precio: 350,
+    CantDisponible: 15
+};
+
+producto.nombre = "Zapatillas"
+
+producto.categoria = "calzado"
+
+delete producto.CantDisponible
+
+console.log (producto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
